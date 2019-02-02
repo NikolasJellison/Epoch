3945b13772b23c02239eb10328a0d9caca0d51ba
Issac Moreno <AbelIMoreno@wireless-10-146-193-46.public.utexas.edu>
2019-02-02 16:31:19 -0600

    Adding Watch Test Scene

---
+++ Assets/Time_Bank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+    public class Time_Bank : MonoBehaviour
+{
+    public UnityEngine.UI.Text timerLabel;
+    public int Amount_Of_Time;
+
+    private float time;
+
+    public void Start()
+    {
+        timerLabel.text = Amount_Of_Time.ToString();
+    }
+
+
+
+    public void  Update_time(DateTime initial_time)
+    {
+
+    }
+
+}
---
+++ Assets/Watch_Vision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Watch_Vision : MonoBehaviour
+{
+    public GameObject vision_Objects;
+    public Time_Bank bank;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vision_Objects.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            vision_Objects.SetActive(true);
+            DateTime startTime = DateTime.Now;
+            bank.Update_time(startTime);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)){
+            vision_Objects.SetActive(false);
+        }
+
+    }
+}
