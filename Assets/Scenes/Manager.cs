93474ee9de0f86e21a9c223c1783a92de028ad4e
JongminRichardKim <JongminRKim@utexas.edu>
2019-02-03 18:26:01 -0600

    Revert "Revert "Implement swapping spawn points upon reaching lockers""
    This reverts commit b2cc9aa7bcb0daea8287c9c3f8667117682c3c82.

---
+++ Assets/Scenes/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public Transform[] spawns;
+    public int currentSpawn;
+    public Transform[] lockers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        /*
+        print(locker.closedFirst);
+        //*/
+        
+        if (currentSpawn != 3)
+        {
+            Locker locker = lockers[currentSpawn+1].GetChild(7).GetChild(1).GetChild(2).GetComponent<Locker>();
+            if(locker.closedFirst)
+            {
+                ++currentSpawn;
+            }
+        }
+
+        // If we fall off the stage or are detected, move an object to that place.
+    }
+}
