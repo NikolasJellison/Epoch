5fe87fa4b8547dc453faca358b948d25b66d266b
Abel Moreno <abel24m@utexas.edu>
2019-02-04 10:00:09 -0600

    Update of last night leftoff

---
+++ Assets/Scripts/EmsyCharacter.cs
@@ -0,0 +1,66 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class EmsyCharacter : MonoBehaviour
+//{
+//    Rigidbody m_Rigidbody;
+//    Animator m_Animator;
+//    bool m_IsGrounded;
+//    float m_CapsuleHeight;
+//    Vector3 m_CapsuleCenter;
+//    CapsuleCollider m_Capsule;
+//    bool m_Crouching; 
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        m_Animator = GetComponent<Animator>();
+//        m_Rigidbody = GetComponent<Rigidbody>();
+//        m_Capsule = GetComponent<CapsuleCollider>();
+//        m_CapsuleHeight = m_Capsule.height;
+//        m_CapsuleCenter = m_Capsule.center;
+
+//        //m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+//        //m_OrigGroundCheckDistance = m_GroundCheckDistance;
+
+//    }
+
+//    public void Move(Vector3 move, bool crouch, bool jump)
+//    {
+
+//        // convert the world relative moveInput vector into a local-relative
+//        // turn amount and forward amount required to head in the desired
+//        // direction.
+//        if (move.magnitude > 1f) move.Normalize();
+//        move = transform.InverseTransformDirection(move);
+//        CheckGroundStatus();
+//        move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+//        m_TurnAmount = Mathf.Atan2(move.x, move.z);
+//        m_ForwardAmount = move.z;
+
+//        ApplyExtraTurnRotation();
+
+//        // control and velocity handling is different when grounded and airborne:
+//        if (m_IsGrounded)
+//        {
+//            HandleGroundedMovement(crouch, jump);
+//        }
+//        else
+//        {
+//            HandleAirborneMovement();
+//        }
+
+//        ScaleCapsuleForCrouching(crouch);
+//        PreventStandingInLowHeadroom();
+
+//        // send input and other state parameters to the animator
+//        UpdateAnimator(move);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
---
+++ Assets/Scripts/EmsyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmsyController : MonoBehaviour
+{
+    public Transform theCamera;
+ 
+    public float movementSpeed;
+    public float jump_power;
+    private Transform myTransform;
+    private float jumped;
+    Animator anim;
+    Rigidbody m_RigidBody;
+ 
+ 
+    void Start()
+    {
+        myTransform = this.transform;
+        m_RigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("OnGround", true);
+    }
+
+    void Update()
+    {
+        // get inputs
+        if (Input.GetButtonDown("Space")){
+            m_RigidBody.AddForce(new Vector3(0, 1, 0));
+        }
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        //float jumped = Input.GetAxis("Jumped");
+
+        // get current position, then do calculations
+        Vector3 moveVectorX = theCamera.forward * inputY;
+        Vector3 moveVectorY = theCamera.right * inputX;
+        //Vector3 moveVectorZ = theCamera.up * jumped;
+        Vector3 moveVector = (moveVectorX + moveVectorY ).normalized * movementSpeed * Time.deltaTime;
+
+        // update Character position
+        myTransform.position = myTransform.position + new Vector3(moveVector.x, 0, moveVector.z);
+
+        // and rotation
+        myTransform.LookAt(myTransform.position + new Vector3(moveVector.x, 0, moveVector.z));
+
+    }
+
+
+}
