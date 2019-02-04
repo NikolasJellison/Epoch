cae1f770319d4f7ad19af42ddc79ee1874523bc7
JongminRichardKim <JongminRKim@utexas.edu>
2019-02-03 19:59:56 -0600

    Update Watch Timekeeping

--- Assets/Scripts/Time_Bank.cs
+++ Assets/Scripts/Time_Bank.cs
@@ -7,17 +7,20 @@ using System;
 public class Time_Bank : MonoBehaviour
 {
     public UnityEngine.UI.Text timerLabel;
+    public int Time_Left;
     public int Amount_Of_Time;
 
 
     public void Start()
     {
         timerLabel.text = Amount_Of_Time.ToString();
+        Time_Left = Amount_Of_Time;
     }
 
     public void Update_time(DateTime initial_time)
     {
-        timerLabel.text = (Amount_Of_Time - (DateTime.Now.Second - initial_time.Second)).ToString();
+        Time_Left = Amount_Of_Time - (DateTime.Now.Second - initial_time.Second);
+        timerLabel.text = Time_Left.ToString();
     }
 
 }
