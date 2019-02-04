8ea362651f825b16b60e2242070f83d18de19d5a
Abel Moreno <abel24m@utexas.edu>
2019-02-03 19:02:12 -0600

    Updated Watch Vision Pre-Fab

--- Assets/Time_Bank.cs
+++ Assets/Time_Bank.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 using System;
 
 
-    public class Time_Bank : MonoBehaviour
+public class Time_Bank : MonoBehaviour
 {
     public UnityEngine.UI.Text timerLabel;
     public int Amount_Of_Time;
 
-    private float time;
 
     public void Start()
     {
         timerLabel.text = Amount_Of_Time.ToString();
     }
 
-
-
-    public void  Update_time(DateTime initial_time)
+    public void Update_time(DateTime initial_time)
     {
-
+        timerLabel.text = (Amount_Of_Time - (DateTime.Now.Second - initial_time.Second)).ToString();
     }
 
 }
--- Assets/Watch_Vision.cs
+++ Assets/Watch_Vision.cs
@@ -7,26 +7,46 @@ public class Watch_Vision : MonoBehaviour
 {
     public GameObject vision_Objects;
     public Time_Bank bank;
+    private Boolean in_Vision;
+    DateTime startTime;
 
     // Start is called before the first frame update
     void Start()
     {
         vision_Objects.SetActive(false);
+        in_Vision = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+
+        if (!in_Vision)
         {
-            vision_Objects.SetActive(true);
-            DateTime startTime = DateTime.Now;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+
+                startTime = DateTime.Now;
+                in_Vision = true;
+                vision_Objects.SetActive(true);
+                bank.Update_time(startTime);
+            }
+
+        }
+        else
+        {
             bank.Update_time(startTime);
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+
+                in_Vision = false;
+                vision_Objects.SetActive(false);
+                bank.Amount_Of_Time = int.Parse(bank.timerLabel.text);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift)){
-            vision_Objects.SetActive(false);
-        }
+
+
+
 
     }
 }
