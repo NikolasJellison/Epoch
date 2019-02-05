6a0e7bf2c92ca0083b8b83e43d30bf53eb2681ad
Abel Moreno <abel24m@utexas.edu>
2019-02-05 11:34:43 -0600

    Updated controller and camera for both scenes
    Her prefab is Character_Camerav1

---
+++ Assets/Scenes/Watch Test/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotation_speed;
+    public Transform target, player;
+    float mouseX, mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        camControl();
+    }
+
+    void camControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotation_speed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotation_speed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ Assets/Scenes/Watch Test/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+
+    private Rigidbody rb;
+    public LayerMask groundLayers;
+    public float Jump_Force = 7;
+    private CapsuleCollider col;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * Jump_Force, ForceMode.Impulse);
+
+        }
+
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playermovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        transform.Translate(playermovement, Space.Self);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center,
+                new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+}
