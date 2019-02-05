6424fa45b5e0b10f5ebfcf7cad3a3d55b21a059a
JongminRichardKim <JongminRKim@utexas.edu>
2019-02-04 22:53:21 -0600

    Fix enemy in level 1, Add transition logic to level 2

---
+++ Assets/Scripts/LivingRoomDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LivingRoomDeath : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemy.GetComponent<EnemyPathing>().alertLevel >= 100)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
