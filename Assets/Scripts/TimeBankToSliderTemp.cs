23dde2363b2da9ab18b5e0af981773ccd0df1ea4
Nikolas <38110784+jjjamer@users.noreply.github.com>
2019-02-13 13:56:42 -0600

    Added canvas to watchTest1, added slider functionality. Moved some animations into the animations folder

---
+++ Assets/Scripts/TimeBankToSliderTemp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBankToSliderTemp : MonoBehaviour
+{
+    public Time_Bank timeBank;
+    public Watch_Vision watchVision;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.maxValue = timeBank.Amount_Of_Time;
+    }
+    private void Update()
+    {
+        slider.value = watchVision.time_left;
+    }
+}
