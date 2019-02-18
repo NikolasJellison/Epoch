346709ba20a7dd4ff048719ad29c1b39986ac96b
Abel Moreno <abel24m@utexas.edu>
2019-02-18 17:44:06 -0600

    Updated pushing and pulling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed;
-
+    public float input_speed;
+    private float manip_speed = 2;
+    private float speed;
     private Rigidbody rb;
     public LayerMask groundLayers;
     public float Jump_Force = 3;
     private CapsuleCollider col;
     private bool lock_movement = false;
     private Animator anim;
+    private bool manipulating = false;
 
     private void Start()
     {
@@ -21,7 +23,7 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
 
 
-
+        speed = input_speed;
     }
     // Update is called once per frame
     void Update()
@@ -54,7 +56,28 @@ public class PlayerController : MonoBehaviour
             }
 
         }
+        else if (other.CompareTag("Move_Able") && !manipulating)
+        {
+            print("Movable Object is close");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                other.transform.parent = transform;
+                anim.SetBool("Manipulating", true);
+                speed = manip_speed;
+                manipulating = true;
+            }
+        }
+        else if (other.CompareTag("Move_Able") && manipulating)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                other.transform.parent = null;
+                anim.SetBool("Manipulating", false);
+                speed = input_speed;
+                manipulating = false;
+            }
 
+        }
     }
 
 
@@ -72,19 +95,23 @@ public class PlayerController : MonoBehaviour
         }
 
         float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical")/2;
+        float ver = Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.LeftShift))
         {
             ver = ver * 2;
         }
 
-        anim.SetFloat("Velocity_X", hor);
-        anim.SetFloat("Velocity_Y", ver);
+
         Vector3 playermovement;
         if (ver < 0f)
-            playermovement = new Vector3(hor, 0f, ver) * speed/2 * Time.deltaTime;
+            playermovement = new Vector3(hor, 0f, ver) * speed / 2 * Time.deltaTime;
         else
+        {
+            ver = ver / 2;
             playermovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        }
+        anim.SetFloat("Velocity_X", hor);
+        anim.SetFloat("Velocity_Y", ver);
 
         transform.Translate(playermovement, Space.Self);
     }
