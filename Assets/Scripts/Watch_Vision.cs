96f730e62b72fdbb01affb5522901c0bb7ebdb5b
Nik <38110784+jjjamer@users.noreply.github.com>
2019-02-18 18:36:40 -0600

    Modified, startToEndPoint into new script startToEndPoint2. Which allows the moving platforms to be controlled from the watch script along with being able to be blocked. Also cleaned up the HallwayWhitebox scene

--- Assets/Scripts/Watch_Vision.cs
+++ Assets/Scripts/Watch_Vision.cs
@@ -5,7 +5,8 @@ using System;
 
 public class Watch_Vision : MonoBehaviour
 {
-    public GameObject vision_objects;
+    //public GameObject vision_objects;
+    public GameObject[] vision_objects;
     public UnityEngine.UI.Text timer_label;
     public float time_left;
     public float original_time;
@@ -13,7 +14,10 @@ public class Watch_Vision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        vision_objects.SetActive(false);
+        //vision_objects.SetActive(false);
+        //Find all the objects you can pull into with vision
+        vision_objects = GameObject.FindGameObjectsWithTag("Pull_Able");
+
         timer_label.text = time_left.ToString();
     }
 
@@ -23,12 +27,28 @@ public class Watch_Vision : MonoBehaviour
         if (Input.GetKey(KeyCode.Mouse1) && time_left > 0.0)
         {
             time_left -= Time.deltaTime;
-            vision_objects.SetActive(true);
+            //vision_objects.SetActive(true);
+
+            //Make the objects move to destination
+
+            //need to change this whenever we figure out how we want to select specific pullable objects
+            foreach(GameObject vision_object in vision_objects)
+            {
+                //using the 2nd version of the script right here 
+                vision_object.GetComponent<startToEndPoint2>().moveToDestination = true;
+            }
 
         }
         if (Input.GetKeyUp(KeyCode.Mouse1) || time_left <= 0.0)
         {
-            vision_objects.SetActive(false);   
+            //vision_objects.SetActive(false);   
+
+            //Return the objects to their home
+            foreach (GameObject vision_object in vision_objects)
+            {
+                //using the 2nd version of the script right here 
+                vision_object.GetComponent<startToEndPoint2>().moveToDestination = false;
+            }
         }
         if(time_left < 0.0)
         {
---
+++ Assets/Scripts/startToEndPoint2.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startToEndPoint2 : MonoBehaviour
+{
+    //Home and Desination
+    [SerializeField]
+    Transform endPoint;
+    private Vector3 startPosition;
+    private Vector3 destination;
+
+    //Movement speed
+    public float moveSpeed = 2;
+
+    public float timeBank = 3;
+    private float timer = 0;
+
+    //
+    private bool isMousePressed = false;
+    //access from time bank script
+    [HideInInspector] public bool moveToDestination;
+    private bool isMovingBack = false;
+
+    //Raycast stuff for blockers
+    public float detectionRange = .5f;
+    private float originalMoveSpeed;
+
+
+     void Start()
+    {
+        //Wherever the object is at the start of runtime, that is its "home"
+        startPosition = transform.position;
+        //default its destination to the start
+        destination = startPosition;
+        //Going to set moveSpeed to 0 when object is blocked, need this to reset it back
+        //Probably a better way to do this
+        originalMoveSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug
+        /*
+        if (Input.GetMouseButtonDown(1))
+        {
+            isMousePressed = true;
+        }
+        */
+
+        if (moveToDestination && transform.position != endPoint.position)
+        {
+            //Move to destination
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, moveSpeed * Time.deltaTime);
+        }
+        else if(!moveToDestination && transform.position != startPosition)
+        {
+            //move home
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, moveSpeed * Time.deltaTime);
+        }
+
+        //Getting the time from the time_bank script now
+        /*
+        if (transform.position == endPoint.position)
+        {
+            moveToDestination = false;
+            timer += Time.deltaTime;
+        }
+        
+        if (timer > timeBank)
+        {
+            isMovingBack = true;
+            if (transform.position != startPosition)
+            {
+                //Move to Home
+                transform.position = Vector3.MoveTowards(transform.position, startPosition, moveSpeed * Time.deltaTime);
+            }
+            if(transform.position == startPosition)
+            {
+                timer = 0;
+                isMovingBack = false;
+            }
+        }
+        */
+
+        //Going to get direction of movement, then raycast and see if something is in front of the direction it is moving
+
+        //Debug here to show where the object's home and destiation are (only shows up in 'scene view' while in runtime
+        //[Blue] = Home (startposition)
+        //[Red] = Destination (endposition)
+        Debug.DrawRay(transform.position, startPosition - transform.position, Color.blue);
+        Debug.DrawRay(transform.position, endPoint.position - transform.position, Color.red);
+
+        RaycastHit hit;
+        if (!moveToDestination)
+        {
+            if (Physics.Raycast(transform.position, startPosition - transform.position, out hit, detectionRange))
+            {
+                //Moving to Home
+                Debug.Log(transform.name + " is stopping because I hit an object with name:" + hit.transform.name + " while trying to move to my home at:" + startPosition);
+                moveSpeed = 0;
+                //Might need to add some kind of check to make sure the object is only stopping when blocked by the correct things
+                //So maybe a compareTag here or something
+            }
+            else
+            {
+                moveSpeed = originalMoveSpeed;
+            }
+        }
+        else if (moveToDestination)
+        {
+            if (Physics.Raycast(transform.position, endPoint.position - transform.position, out hit, detectionRange))
+            {
+                //Moving to Destination
+                Debug.Log(transform.name + " is stopping because I hit an object with name:" + hit.transform.name + " while trying to move to my destination, named:" + endPoint.name + ". Which is located at:" + endPoint.position);
+                moveSpeed = 0;
+            }
+            else
+            {
+                moveSpeed = originalMoveSpeed;
+            }
+        }
+    }
+}
