08f92094af64029df1aa5bbdc7ddb50c80ef58cc
JongminRichardKim <JongminRKim@utexas.edu>
2019-02-21 20:19:55 -0600

    Added proposal code for new Vision Mechanic

---
+++ Assets/Scenes/Proposal/PerspectiveScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float zAngle;
+    public float zTarget;
+    public float zLeeway;
+    public bool active;
+    public Material visionMat;
+    public Material realMat;
+    public Transform groundBase;
+    public GameObject marker;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(groundBase == null)
+        {
+            groundBase = transform;
+        }
+        gameObject.GetComponent<MeshRenderer>().material = visionMat;
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toObject = groundBase.position - player.position;
+        Debug.DrawRay(player.position, toObject, Color.green);
+        zAngle = Vector3.SignedAngle(toObject, groundBase.forward, groundBase.up);
+
+        float zDiff = angleDiff(zAngle, zTarget);
+        
+        Color color = gameObject.GetComponent<MeshRenderer>().material.color;
+        color.a = Mathf.Max(0.1f, 0.85f * ((180.0f - zDiff)/180.0f));
+        gameObject.GetComponent<MeshRenderer>().material.color = color;
+
+        if (zDiff < zLeeway)
+        {
+            active = true;
+        } else
+        {
+            active = false;
+        }
+    }
+    
+    private float angleDiff(float angle, float target)
+    {
+        float diff = (target - angle);
+        if (diff > 180.0f)
+        {
+            diff -= 360.0f;
+        }
+        else if (diff < -180.0f)
+        {
+            diff += 360.0f;
+        }
+        diff = Mathf.Abs(diff);
+
+        return diff;
+    }
+
+    private void OnDisable()
+    {
+        marker.SetActive(false);
+    }
+}
---
+++ Assets/Scenes/Proposal/Vision.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    public GameObject mouseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool activeUI = false;
+        //bool decreasePower = false;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if (hit.transform.CompareTag("Vision"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                PerspectiveScript pScript = hit.transform.gameObject.GetComponent<PerspectiveScript>();
+                if (pScript.enabled && pScript.active && hit.distance > 2.2f)
+                {
+                    activeUI = true;
+                    // print("OOH YEAH");
+                    
+                    // on down: set current target
+                    // on held: add power
+                    // on release: reset current target, reset power
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        // Change colliders to be interactable
+                        if (hit.collider.GetType() == typeof(MeshCollider))
+                        {
+                            MeshCollider mCollider = (MeshCollider)hit.collider;
+                            mCollider.isTrigger = false;
+                            mCollider.convex = false;
+                        }
+                        else
+                        {
+                            hit.collider.isTrigger = false;
+                        }
+
+                        // Alter materials as necessary
+                        Material realMat = pScript.realMat;
+                        hit.transform.gameObject.GetComponent<MeshRenderer>().material = realMat;
+                        // Disable the PerspectiveScript component
+                        hit.transform.gameObject.GetComponent<PerspectiveScript>().enabled = false;
+                        // Activate any components necessary for special effects
+                    }
+                }
+            } else
+            {
+                //decreasePower = true;
+               // print("Oh.");
+                // power = 0;
+            }
+            if(activeUI)
+            {
+                //hit.transform.gameObject.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            } else
+            {
+                //hit.transform.gameObject.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+            }
+            mouseUI.SetActive(activeUI);
+        }
+    }
+}
