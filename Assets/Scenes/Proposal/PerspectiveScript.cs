793b604dbc4514fe11d0ef3db5960eae90fc61a1
JongminRichardKim <JongminRKim@utexas.edu>
2019-02-24 02:01:20 -0600

    Updated Perspective Vision Math
    - Less complex, more accurate, more flexible vision code
    - Also added meta files

--- Assets/Scenes/Proposal/PerspectiveScript.cs
+++ Assets/Scenes/Proposal/PerspectiveScript.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PerspectiveScript : MonoBehaviour
-{
-
+{ 
     public Transform player;
-    public float zAngle;
-    public float zTarget;
-    public float zLeeway;
+    public Vector3 ray;
+    public Vector3 targetRay;
+    public float leeway;
+    public float diff;
     public bool active;
+    public float distance;
+    public float targetDist;
+    public float distLeeway;
     public Material visionMat;
     public Material realMat;
-    public Transform groundBase;
     public GameObject marker;
-    
     
     // Start is called before the first frame update
     void Start()
     {
-        if(groundBase == null)
-        {
-            groundBase = transform;
-        }
         gameObject.GetComponent<MeshRenderer>().material = visionMat;
         active = false;
     }
@@ -30,40 +27,23 @@ public class PerspectiveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 toObject = groundBase.position - player.position;
-        Debug.DrawRay(player.position, toObject, Color.green);
-        zAngle = Vector3.SignedAngle(toObject, groundBase.forward, groundBase.up);
-
-        float zDiff = angleDiff(zAngle, zTarget);
-        
+        ray = Vector3.Normalize(transform.position - player.position);
+        distance = Vector3.Distance(transform.position, player.position);
+        diff = Vector3.Angle(ray, targetRay);
         Color color = gameObject.GetComponent<MeshRenderer>().material.color;
-        color.a = Mathf.Max(0.1f, 0.85f * ((180.0f - zDiff)/180.0f));
+        color.a = Mathf.Max(0.1f, 0.85f * ((180.0f - diff) / 180.0f));
         gameObject.GetComponent<MeshRenderer>().material.color = color;
-
-        if (zDiff < zLeeway)
+        
+        if (diff < leeway && Mathf.Abs(distance - targetDist) < distLeeway)
         {
             active = true;
-        } else
+        }
+        else
         {
             active = false;
         }
     }
-    
-    private float angleDiff(float angle, float target)
-    {
-        float diff = (target - angle);
-        if (diff > 180.0f)
-        {
-            diff -= 360.0f;
-        }
-        else if (diff < -180.0f)
-        {
-            diff += 360.0f;
-        }
-        diff = Mathf.Abs(diff);
 
-        return diff;
-    }
 
     private void OnDisable()
     {
