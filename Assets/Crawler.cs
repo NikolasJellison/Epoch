da0d013479e7e99905054242182d6a3c52755303
Abel Moreno <am75343@lib-fabfm005.austin.utexas.edu>
2019-04-01 19:15:03 -0500

    Adding Crawling scripts

---
+++ Assets/Crawler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawler : MonoBehaviour
+{
+	private BoxCollider box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
