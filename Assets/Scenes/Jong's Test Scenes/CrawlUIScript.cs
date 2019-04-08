8f0792fe3444e642175d981490baeadf9ce1151d
JongminRichardKim <JongminRKim@utexas.edu>
2019-04-08 01:51:25 -0500

    Add crawl pads to level 1 with UI
    Also adjusted placement of chair

---
+++ Assets/Scenes/Jong's Test Scenes/CrawlUIScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrawlUIScript : MonoBehaviour
+{
+    public Text crawlUI;
+    public PerspectiveSwap vantageMgr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crawlUI.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            crawlUI.text = "'C' to crawl";
+        }
+        if (!vantageMgr.playerActive)
+        {
+            crawlUI.text = "";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            crawlUI.text = "'C' to crawl";
+        }
+        if (!vantageMgr.playerActive)
+        {
+            crawlUI.text = "";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            crawlUI.text = "";
+        }
+    }
+}
