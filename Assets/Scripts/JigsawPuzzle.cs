2b67b2d019edeb434e3210a88a581c76ea2eccec
Nik <38110784+jjjamer@users.noreply.github.com>
2019-04-14 19:08:31 -0500

    Updated JigSaw Puzzle. Should be resized and now has outlines. Also scattered the pieces a bit. New detection transforms which will allow for easy movement for locked pieces if needed

--- Assets/Scripts/JigsawPuzzle.cs
+++ Assets/Scripts/JigsawPuzzle.cs
@@ -1,23 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class JigsawPuzzle : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+public class JigsawPuzzle : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private Vector3 originalPosition;
     private bool locked;
     private bool moving;
     public JigSawManager jigSawManager;
+    private Outline outline;
+    private Vector3 offset;
 
     private void Start()
     {
         originalPosition = transform.position;
+        outline = GetComponent<Outline>();
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (!locked)
+        {
+            var tempColor = outline.effectColor;
+            tempColor.a = 66;
+            outline.effectColor = tempColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (!locked)
+        {
+            var tempColor = outline.effectColor;
+            tempColor.a = 0;
+            outline.effectColor = tempColor;
+        }
     }
 
     public void OnPointerDown(PointerEventData pointerEventData)
     {
         //Shader stuff goes here i suppose
+
+        //Put selected image on top (this is hardcoded index because there are 10 pieces
+        if (!locked)
+        {
+            transform.SetSiblingIndex(9);
+            //Offset
+            offset = transform.position - Input.mousePosition;
+        }
     }
 
     public void OnDrag(PointerEventData data)
@@ -25,7 +57,7 @@ public class JigsawPuzzle : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
         if (!locked)
         {
             moving = true;
-            transform.position = Input.mousePosition;
+            transform.position = Input.mousePosition + offset;
         }
     }
 
@@ -53,6 +85,12 @@ public class JigsawPuzzle : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
             if (!locked)
             {
                 jigSawManager.CorrectPlacement();
+                //Get rid out outline
+                var tempColor = outline.effectColor;
+                tempColor.a = 0;
+                outline.effectColor = tempColor;
+                //Send to back to avoid hiding any pieces
+                transform.SetSiblingIndex(0);
             }
             locked = true;
         }
