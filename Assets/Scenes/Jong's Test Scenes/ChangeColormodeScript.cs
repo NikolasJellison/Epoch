6770eb47639abe0b7b6dc9cf6b3586acd066ff2d
JongminRichardKim <JongminRKim@utexas.edu>
2019-04-16 03:13:45 -0500

    Implement mode swapping, ready to test

--- Assets/Scenes/Jong's Test Scenes/ChangeColormodeScript.cs
+++ Assets/Scenes/Jong's Test Scenes/ChangeColormodeScript.cs
@@ -5,22 +5,63 @@ using UnityEngine;
 public class ChangeColormodeScript : MonoBehaviour
 {
     public List<MeshRenderer> sceneMRs;
+    public List<SkinnedMeshRenderer> sceneSMRs;
     public List<Material> sceneMats;
+    public bool dirty;
+    public float currentMode;
     // Start is called before the first frame update
     
     void Start()
     {
+        currentMode = 0;
+        dirty = false;
         sceneMRs = new List<MeshRenderer>(GameObject.FindObjectsOfType<MeshRenderer>());
+        sceneSMRs = new List<SkinnedMeshRenderer>(GameObject.FindObjectsOfType<SkinnedMeshRenderer>());
+        foreach (SkinnedMeshRenderer rend in sceneSMRs)
+        {
+            foreach (Material mat in rend.materials)
+            {
+                if (!sceneMats.Contains(mat))
+                {
+                    if (!sceneMats.Contains(mat))
+                    {
+
+                        if (mat.shader.name == "Shader Graphs/ColorBlindTest")
+                        {
+                            sceneMats.Add(mat);
+                        }
+                        /*
+                        else
+                        {
+                            string message = rend.gameObject.name + ": " + mat.name + ", " + mat.shader.name;
+                            if (rend.transform.parent != null)
+                            {
+                                message = rend.transform.parent.name + "/" + message;
+                            }
+                            print(message);
+                        }
+                        //*/
+                    }
+                }
+            }
+
+        }
+
         foreach (MeshRenderer rend in sceneMRs)
         {
             foreach(Material mat in rend.materials)
             {
                 if (!sceneMats.Contains(mat))
                 {
-                    sceneMats.Add(mat);
-                    if (!(mat.shader.name == "Shader Graphs/ColorBlindTest"))
+                    
+                    if(mat.shader.name == "Shader Graphs/ColorBlindTest")
                     {
-                        string message = rend.gameObject.name + ": " + mat.shader.name;
+                        sceneMats.Add(mat);
+                    }
+                    /*
+                    else
+                    {
+                        string message = rend.gameObject.name + ": " + mat.name +", " + mat.shader.name;
                         if(rend.transform.parent != null)
                         {
                             message = rend.transform.parent.name + "/" + message;
@@ -29,27 +70,47 @@ public class ChangeColormodeScript : MonoBehaviour
                         
 
                     }
+                    //*/
                     //print();
                 }
             }
             
         }
-        //sceneMats = new List<Material>(GameObject.FindObjectsOfType<Material>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //sceneMRs[0].game
-        /*if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            
-            foreach(MeshRenderer mr in sceneMRs)
-            {
-                //mr.GetMaterials();
-            }
-            
+            currentMode = 0.0f;
+            dirty = true;
         }
-    //*/
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentMode = 1.0f;
+            dirty = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentMode = 2.0f;
+            dirty = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentMode = 3.0f;
+            dirty = true;
+        }
+        if (dirty)
+        {
+            foreach (Material mat in sceneMats)
+            {
+                mat.SetFloat("_colorblind", currentMode);
+            }
+            dirty = false;
+        }
     }
 }
