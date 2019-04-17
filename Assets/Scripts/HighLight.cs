5eb8c077aa342e6c8c66348c9ac00a4b555441b0
Nikolas <38110784+jjjamer@users.noreply.github.com>
2019-04-17 15:27:10 -0500

    "Fixed" particles on title screen. Start of highlight testing. Added new memory to shuffle puzzle

---
+++ Assets/Scripts/HighLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighLight : MonoBehaviour
+{
+    public GUISkin skin;
+    public Color highlightColor = Color.cyan;
+    private Color originalColor;
+
+    private void OnGUI()
+    {
+        GUI.skin = skin;
+    }
+
+    private void OnMouseEnter()
+    {
+        originalColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = highlightColor;
+    }
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+}
