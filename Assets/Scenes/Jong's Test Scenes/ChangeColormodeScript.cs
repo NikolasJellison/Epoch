2937607f31a64c951794a2d6966ed0af6292a07d
JongminRichardKim <JongminRKim@utexas.edu>
2019-04-17 06:02:39 -0500

    Various adjustments for colorblind mode

--- Assets/Scenes/Jong's Test Scenes/ChangeColormodeScript.cs
+++ Assets/Scenes/Jong's Test Scenes/ChangeColormodeScript.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChangeColormodeScript : MonoBehaviour
 {
     public List<MeshRenderer> sceneMRs;
     public List<SkinnedMeshRenderer> sceneSMRs;
     public List<Material> sceneMats;
-    public GameObject[] particles;
+    
     public Color[] particleColors;
     private List<Material> objectiveMats = new List<Material>();
     // Color: 0 = Reguar, 1 = Deuteranopia, 2 = Protanopia, 3 = Tritanopia
@@ -16,20 +17,25 @@ public class ChangeColormodeScript : MonoBehaviour
     public Material visionMat;
     public Color[] visionColors = new Color[4];
     // level 1
-    // particle effects
+    public TextMeshProUGUI tutorialText;
+    public GameObject[] particles;
     // Level 2
-    public Material hallwayLights;
+    public List<Material> hallwayLights = new List<Material>();
     public Color[] hallwayLightColors = new Color[4];
-    public List<Light> lights;
-    public Color[] lightColors = new Color[4];
+    public List<Light> lights = new List<Light>();
+    //public Color[] lightColors = new Color[4];
     public List<Text> text;
     public Color[] textColors = new Color[4];
+    public Light objectiveLight;
     // Level 3
-    public List<Material> waterMats;
+    public List<MeshRenderer> waterMats;
     public Color[] waterColors = new Color[4];
+    public Material stopper;
+
     // 
     public bool dirty;
     public float currentMode;
+
     
     void Start()
     {
@@ -69,17 +75,20 @@ public class ChangeColormodeScript : MonoBehaviour
         {
             foreach(Material mat in rend.materials)
             {
-                if (!sceneMats.Contains(mat))
+                if (mat.shader.name == "Shader Graphs/Objectives")
                 {
-                    if (mat.shader.name == "Shader Graphs/Objectives")
-                    {
-                        objectiveMats.Add(mat);
-                    }
+                    objectiveMats.Add(mat);
+                }
+                if (mat.name == "Hallway Light (Instance)" || mat.name == "Hallway Light")
+                {
+                    hallwayLights.Add(mat);
+                }
+                if (!sceneMats.Contains(mat))
+                {   
                     if (mat.shader.name == "Shader Graphs/ColorBlindTest")
                     {
                         sceneMats.Add(mat);
                     }
-                    
                     else
                     {
                         string message = rend.gameObject.name + ": " + mat.name +", " + mat.shader.name;
@@ -141,7 +150,12 @@ public class ChangeColormodeScript : MonoBehaviour
             {
                 t.color = textColors[index];
             }
-            if(particles.Length > 0)
+            if (tutorialText != null)
+            {
+                tutorialText.color = textColors[index];
+            }
+
+            if (particles.Length > 0)
             {
                 foreach(GameObject pSystem in particles)
                 {
@@ -151,6 +165,36 @@ public class ChangeColormodeScript : MonoBehaviour
             }
             
             //objectiveMat.
+            if(hallwayLights.Count > 0)
+            {
+                foreach(Material light in hallwayLights)
+                {
+                    if(light != null)
+                    {
+                        light.SetColor("_EmissionColor", hallwayLightColors[index]);
+                    }
+                    
+                }
+            }
+            if (lights.Count > 0)
+            {
+                foreach(Light light in lights)
+                {
+                    if (light != null)
+                    {
+                        light.color = hallwayLightColors[index];
+                    }
+                }
+            }
+            if(objectiveLight != null)
+            {
+                objectiveLight.color = objectiveColors[index];
+            }
+            if(stopper != null)
+            {
+                stopper.SetColor("_color", visionColors[index]);
+            }
+            //foreach()
             dirty = false;
         }
 
