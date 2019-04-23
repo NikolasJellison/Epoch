0095a9ec8998d0e3f6f485f87960f11957dd53b9
Nik <38110784+jjjamer@users.noreply.github.com>
2019-04-23 02:16:25 -0500

    Poorly made cutscene transitions. Animations are so bugged.... everything should work. Both scenes needed a new controller or something idk camera angles were way off

---
+++ Assets/Level3CutSceneWait.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3CutSceneWait : MonoBehaviour
+{
+    public PlayerController playerC;
+
+    private void OnEnable()
+    {
+        playerC.lock_movement = true;
+        playerC.gameObject.SetActive(false);
+    }
+    private void OnDisable()
+    {
+        playerC.lock_movement = false;
+        playerC.gameObject.SetActive(true);
+    }
+}
