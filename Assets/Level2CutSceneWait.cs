f968df9ebec020cffe6eb522b95707b2dc0360a8
Nik <38110784+jjjamer@users.noreply.github.com>
2019-04-23 23:06:32 -0500

    Level 2 cutscene refined to meet current story board (kinda). Still some bugs:
    Player controller spawns with rotation 0,0,0 no matter if i try and change it. Morph is still weird. Light room worked most of the time but still glitched sometimes.

--- Assets/Level2CutSceneWait.cs
+++ Assets/Level2CutSceneWait.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Level2CutSceneWait : MonoBehaviour
 {
     public PlayerController playerC;
-
+    public GameObject teenModelCutscene;
+    private Transform t;
+    public GameObject pointLight3;
     private void OnEnable()
     {
         playerC.lock_movement = true;
@@ -15,5 +17,11 @@ public class Level2CutSceneWait : MonoBehaviour
     {
         playerC.lock_movement = false;
         playerC.gameObject.SetActive(true);
+        playerC.gameObject.transform.position = teenModelCutscene.transform.position;
+        //hard code because this is taking too long
+        Debug.Log(playerC.gameObject.transform.eulerAngles);
+        playerC.gameObject.transform.eulerAngles =  new Vector3(0f, 90f, 0f);
+        Debug.Log(playerC.gameObject.transform.eulerAngles);
+        pointLight3.SetActive(true);
     }
 }
---
+++ Assets/Scripts/CutsceneTransitionGetAlpha.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTransitionGetAlpha : MonoBehaviour
+{
+    private List<Renderer> r = new List<Renderer>();
+    public float alpha = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = new List<Renderer> (GetComponentsInChildren<Renderer>());
+    }
+    private void Update()
+    {
+        foreach(Renderer r in r)
+        {
+            r.material.SetFloat("_alpha", alpha);
+        }
+    }
+}
