150924edc015a9de94cd3e7b21366f5698563a27
JongminRichardKim <JongminRKim@utexas.edu>
2019-05-01 14:21:09 -0500

    Make destructable walls actually stop people until they are destroyed

---
+++ Assets/Scenes/Jong's Test Scenes/DestroyBarrierScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBarrierScript : MonoBehaviour
+{
+    public DestroyPromptScript destroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destroy.triggeredDestruction)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
