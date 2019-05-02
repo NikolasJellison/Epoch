abfe0808218d187a12fe00748406e7e6a2f4012f
Nikolas <38110784+jjjamer@users.noreply.github.com>
2019-05-01 20:04:36 -0500

    Audio Sound test scene

---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+
+    public AudioSource[] audioStuff;
+    public AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (AudioSource a in audioStuff)
+            {
+                a.Play();
+            }
+        }
+    }
+}
