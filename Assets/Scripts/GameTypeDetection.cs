8a81f267d8fb3f26e6ac405b75eb62ef829520e1
Nik <38110784+jjjamer@users.noreply.github.com>
2019-05-07 18:49:21 -0500

    WebGL detection to disable the exit buttons. Script is GameTypeDetection. There is also a prefab called the same thing.

---
+++ Assets/Scripts/GameTypeDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTypeDetection : MonoBehaviour
+{
+    //We detect if the player is in WebGl. If so, then we disable the exit buttons 
+    private GameObject exitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitButton = GameObject.Find("Exit");
+        if (Application.platform == RuntimePlatform.WebGLPlayer && exitButton.GetComponent<Button>() !=null)
+        {
+            exitButton.SetActive(false);
+        }
+    }
+}
