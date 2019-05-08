362f4f48fdb27c16ab82d9450310cc0c60e2b118
JongminRichardKim <JongminRKim@utexas.edu>
2019-05-07 19:54:09 -0500

    Add GameTypeDetection to relevant scenes, minor volume attenuation

--- Assets/Scripts/GameTypeDetection.cs
+++ Assets/Scripts/GameTypeDetection.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 public class GameTypeDetection : MonoBehaviour
 {
     //We detect if the player is in WebGl. If so, then we disable the exit buttons 
-    private GameObject exitButton;
+    public GameObject exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        exitButton = GameObject.Find("Exit");
-        if (Application.platform == RuntimePlatform.WebGLPlayer && exitButton.GetComponent<Button>() !=null)
+        //exitButton = GameObject.Find("Exit");
+        if (Application.platform == RuntimePlatform.WebGLPlayer && exitButton != null && exitButton.GetComponent<Button>() !=null)
         {
             exitButton.SetActive(false);
         }
